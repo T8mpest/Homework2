45217883018ca4ef91c02db10cfd2674f2737ea6
T8mpest <opcache@gmail.com>
2023-06-08 03:01:46 +0300

    adding readline :D
    that was pretty hard, i was google this solution(about readline for int) for 20 min :)

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,10 +1,13 @@
-﻿
-int val = 75348;
+﻿string x = Console.ReadLine();
+int val = Convert.ToInt32(x);
+
+
 int firstVal = val / 10000; // 7 
 int secondVal = val / 1000 - (firstVal * 10); // 5
 int thirdVal = val / 100 - (firstVal * 100 + secondVal * 10); // 3  
 int forthVal = val / 10 - (firstVal * 1000 + secondVal * 100 + thirdVal * 10);
 int fivesVal = val - (firstVal * 10000 + secondVal * 1000 + thirdVal * 100 + forthVal * 10);
+
 
 Console.WriteLine(firstVal);
 Console.WriteLine(secondVal);
@@ -12,8 +15,4 @@ Console.WriteLine(thirdVal);
 Console.WriteLine(forthVal);
 Console.WriteLine(fivesVal);
 Console.ReadLine();
-//Console.WriteLine(val / 100);
-//Console.WriteLine(val / 10 - (val / 100 * 10));
-//Console.WriteLine(val - (val / 10 *10));
 
-//Console.WriteLine(val / 100);
