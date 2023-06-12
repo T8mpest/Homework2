61e0f5e1701d5bae6158755eb40a1d3159a1b6f1
T8mpest <opcache@gmail.com>
2023-06-12 14:56:39 +0300

    again upgrade

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,11 +1,21 @@
 ﻿string x = Console.ReadLine();
 int val = Convert.ToInt32(x);
+var condition = val >= 10000 && val <= 99999; 
+if (condition)
+{
 
-int firstVal = val / 10000; 
-int secondVal = val / 1000 - (firstVal * 10); 
-int thirdVal = val / 100 - (firstVal * 100 + secondVal * 10);  
-int forthVal = val / 10 - (firstVal * 1000 + secondVal * 100 + thirdVal * 10);
-int fivesVal = val - (firstVal * 10000 + secondVal * 1000 + thirdVal * 100 + forthVal * 10);
-Console.WriteLine($"{firstVal} {secondVal} {thirdVal} {forthVal} {fivesVal}");
-Console.ReadLine();
-// i dont understand about spaces in home work , what does it mean, and am i doing right ? i hope yeah, if not pls say me what`s wrong , thank u
+    int firstVal = val / 10000;
+    int secondVal = val / 1000 - (firstVal * 10);
+    int thirdVal = val / 100 - (firstVal * 100 + secondVal * 10);
+    int forthVal = val / 10 - (firstVal * 1000 + secondVal * 100 + thirdVal * 10);
+    int fivesVal = val - (firstVal * 10000 + secondVal * 1000 + thirdVal * 100 + forthVal * 10);
+    Console.WriteLine($"{firstVal} {secondVal} {thirdVal} {forthVal} {fivesVal}");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine($"Error.The number {val} not in a range [10000-99999]");
+    Console.ReadLine();
+}
+
+
